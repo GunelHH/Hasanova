5075c4a3fb5b3ea2738dc95363fe8cfc1fc7a98d
GunelHH <98384542+GunelHH@users.noreply.github.com>
2022-02-09 03:35:42 +0400

    2 ci task

---
+++ Task2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string number {"12,22,10,4"};
+            {
+                if ( "number"[i]%2==0)
+                {
+                    Console.WriteLine("cut");
+                }
+                else
+                {
+                    Console.WriteLine("tek");
+                }
+            }
+
+
+        }
+    }
+}
